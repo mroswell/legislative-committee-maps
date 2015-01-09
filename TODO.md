14576a631ff9990af70465bf165208b79edccbf7
Margie Roswell <mroswell@gmail.com>
2015-01-09 14:36:33 -0500

    place committees into buttons

---
+++ TODO.md
@@ -0,0 +1,12 @@
+- use url parameter to determine state
+- research the various state names, and use state url parameter and json file to determine the names for "House and
+Senate";
+- alphabetize committees within dropdown
+- split large committee lists into two or three columns?
+- Do membership queries for each member, and attach results to the committee object.
+- generate geojson files
+- Leaflet
+- figure out topojson?
+
+Farther along
+- add subcomittees and permalinks
---
+++ js/script.js
@@ -0,0 +1,49 @@
+var populateDropDown= function(state) {
+  var committeeListRequest = $.ajax({
+    dataType: "json",
+    url: "http://openstates.org/api/v1/committees/",
+    data: {
+      state: state,
+      apikey: "9e3e71730ae34e1ebbf4dd0e1c346c07"
+    }
+  }).done(function(committeeList) {
+      committees = _.reject(committeeList, 'subcommittee');
+      upperCommittees = _.where(committees, {chamber: "upper"});
+      lowerCommittees = _.where(committees,{chamber:"lower"});
+//      $('pre').html(JSON.stringify(upperCommittees, null, 2));
+//      for (committeeObj in upperCommittees){
+//        console.log(upperCommittees[committeeObj].committee, " - ", upperCommittees[committeeObj].id)
+//      }
+      var upperOutput = "";
+        $.each(upperCommittees, function(key, val) {
+        upperOutput += '<li><a href="#"  data-committee="' + val.id +'">' + val.committee + '</a></li>';
+      });
+      var lowerOutput = "";
+        $.each(lowerCommittees, function(key, val) {
+        lowerOutput += '<li><a href="#"  data-committee="' + val.id +'">' + val.committee + '</a></li>';
+      });
+      $('ul#tinyDropUpper').prepend(upperOutput);
+      $('ul#tinyDropLower').prepend(lowerOutput);
+
+    })
+};
+
+
+var drawMap= function (committee) {
+  console.log(committee);
+  var committeeRequest = $.ajax({
+    dataType: "json",
+    url: "http://openstates.org/api/v1/committees/" + committee,
+    data: {
+      apikey: "9e3e71730ae34e1ebbf4dd0e1c346c07"
+    }
+  }).done(function(committee){
+      console.log("Committee", committee);
+      console.log(committee.members);
+      leg_id_array = _.pluck(committee.members, 'leg_id');
+      console.log(leg_id_array);
+      $('#update').html(committee);
+    });
+};
+
+
